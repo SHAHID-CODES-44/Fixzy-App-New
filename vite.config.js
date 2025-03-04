195f62a008153249e1a73cb1ac14532d8f68f32f
SHAHID-CODES-44 <shahidfirozaba@gmail.com>
2025-03-04 11:04:07 +0530

    Updated index.html, package.json, and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Fixzy-App/", // 👈 Replace with your repo name if different
+  base: '/Fixzy-App-New/', // 👈 Add this line
 });
